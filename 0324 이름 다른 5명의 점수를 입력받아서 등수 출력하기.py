2fc0ad8617525114ec0c6f9d8da98270406267ba
hannykee <hchw1904@gmail.com>
2018-03-24 14:05:48 +0900

    dailym

--- 0324 이름 다른 5명의 점수를 입력받아서 등수 출력하기.py
+++ 0324 이름 다른 5명의 점수를 입력받아서 등수 출력하기.py
@@ -1,15 +1,25 @@
-#0324 5명의 점수를 입력받아서 등수 출력하기
+#0324 5명의 점수를 입력받아서 등수 출력하기(딕셔너리 저장해보기,리스트변환)
 
-name_score={}
-
+name_score={} 
+data=[]
+name=[]
 for n in range (0,5):
     a=str(input("이름,점수를 입력하세요. 예:기쁨이,90"))
     b=a.split(",")
-    name_score.append(b[0]:b[1])
+    name_score[(b[0])]=int(b[1]) #딕셔너리 저장해보기
+    name.append(b[0]) #리스트 변환
+    data.append(int(b[1]))
 
-for k in range(0,len(name_score)):
+#참고용  set(name_score) #set은 키 값만 추출해서 {}형태로 가진다.
+
+for k in range(0,5):
     order=6
-    for m in range(0,len(score)):
-        if name_score[k번째 키값] >= score[m번째 키값]:
+    for m in range(0,5):
+        if data[k] >= data[m]:
             order-=1
-    print("%d번째 선수의 점수는 %s이고, 등수는 %d등 입니다." %(k+1,name_score[k번째 키값],order))
+    print("%s 선수의 점수는 %d이고, 등수는 %d등 입니다." %(name[k],data[k],order))
+
+
+
+#정렬한채로 인쇄하기
+#data.sort()
