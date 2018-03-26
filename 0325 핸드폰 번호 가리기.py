fc07e1e47fa558a49609ef28b686eb3c17dfb3d9
hannykee <hchw1904@gmail.com>
2018-03-26 23:49:42 +0900

    dailym

--- 0325 핸드폰 번호 가리기.py
+++ 0325 핸드폰 번호 가리기.py
@@ -2,8 +2,14 @@
 
 def hide_numbers():
 	p_num= str(input("핸드폰 번호를 입력해주세요."))
-	for i in range(0,len(p_num)-4):
-		p_num[i]="*"
+	print(p_num[3])
+	for i in range(0,(len(p_num)-4)):
+		p_num[i] = "*"
+
+        number=p_num.split("-")
 	return p_num
 
-print("결과 : " + hide_numbers())
+print("결과 : " , hide_numbers())
+
+
+
