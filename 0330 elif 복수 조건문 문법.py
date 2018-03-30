ea9880618a63539befb126059ad346f6913dd5f4
hannykee <hchw1904@gmail.com>
2018-03-30 23:11:06 +0900

    dailym

---
+++ 0330 elif 복수 조건문 문법.py
@@ -0,0 +1,13 @@
+#0330 elif로 복수 조건문 만들기- 점프 투 파이썬 참고
+
+"주머니에 돈이 있으면 택시를 타고, 주머니에 돈은 없지만 카드가 있으면 택시를 타고, 돈도 없고 카드도 없으면 걸어 가라"
+
+
+pocket = ['paper', 'cellphone']
+card = 1
+if 'money' in pocket:
+     print("택시를 타고가라")
+elif card: 
+     print("택시를 타고가라")
+else:
+     print("걸어가라")
