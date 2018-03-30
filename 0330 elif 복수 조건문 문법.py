6d51c8e7de2d3589865e4b68c872a713c35dc745
hannykee <hchw1904@gmail.com>
2018-03-30 23:39:44 +0900

    dailym

--- 0330 elif 복수 조건문 문법.py
+++ 0330 elif 복수 조건문 문법.py
@@ -1,10 +1,18 @@
-#0330 elif로 복수 조건문 만들기- 점프 투 파이썬 참고
+#0330 elif로 다중 조건 판단문  만들기- 점프 투 파이썬 참고
 
 "주머니에 돈이 있으면 택시를 타고, 주머니에 돈은 없지만 카드가 있으면 택시를 타고, 돈도 없고 카드도 없으면 걸어 가라"
 
+a=input("주머니에 든 것을 나열하세요. ex) paper,cellphone...")
+a=a.replace(' ','')
+pocket=a.split(",")
 
+card=int(input("카드가 있다면 1, 없으면 0을 입력하세요."))
+
+'''
 pocket = ['paper', 'cellphone']
 card = 1
+'''
+
 if 'money' in pocket:
      print("택시를 타고가라")
 elif card: 
