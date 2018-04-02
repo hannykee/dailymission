777b5ad575edd5e0737c55ce390d8739be1d784c
hannykee <hchw1904@gmail.com>
2018-04-02 23:17:44 +0900

    dailym

---
+++ 0402 날짜 문자열 슬라이싱.py
@@ -0,0 +1,9 @@
+#0402 파이썬 슬라이싱(날짜정보 문자열 나누기)
+
+a= "20180402Sunny"
+year=a[0:4] #슬라이싱에서 끝번호는 해당 안됨 0 <= a < 5
+month=a[4:6]
+day=a[6:8]
+weather=a[8:]
+
+print("오늘은 %s,%s,%s,%s 입니다" %(year,month,day,weather))
