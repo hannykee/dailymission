d8ed53caeaf7d23c049f57d0e91fe20f20c12442
hannykee <hchw1904@gmail.com>
2018-04-04 00:06:54 +0900

    debugging

--- 0325 문자열 내 마음대로 정렬하기.py
+++ 0325 문자열 내 마음대로 정렬하기.py
@@ -5,11 +5,23 @@
 #예를들어 strings가 [sun, bed, car]이고 n이 1이면 각 단어의 인덱스 1인 문자 u, e ,a를 기준으로 정렬해야 하므로 결과는 [car, bed, sun]이 됩니다.
 #strange_sort함수를 완성해 보세요
 
+
+def strange_sort(strings,n) :
+        
+        strings.sort(key=str.lower)
+        print(strings)
+        return strings
+
 a=str(input("원하는 문자열을 나열하여 입력하세요."))
 a=a.replace(" ","")
 strings=a.split(",")  #split은 list로 반환
+n=int(input("원하는 인덱스를 설정하세요."))
+strange_sort(strings,n)
 
-def strange_sort() :
-        n=int(input("원하는 인덱스를 설정하세요."))
-        result=strings.sort(key=strings[n])
-        print(result)
+
+
+
+#https://wayhome25.github.io/python/2017/03/07/key-function/ 딕셔너리 정렬 키 값 설정하기
+#본문 https://docs.python.org/3/howto/sorting.html?highlight=sorting#key-functions
+
+#iterable 정의 http://bluese05.tistory.com/55
