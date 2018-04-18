45f952a275f6f829c52b05528e2a214b1145a977
hannykee <hchw1904@gmail.com>
2018-04-18 23:25:27 +0900

    dailym

---
+++ 0418 리스트놀이.py
@@ -0,0 +1,8 @@
+#0418 리스트 놀이
+a=[1,3,5,4,2]
+a.sort
+a.reverse
+print(a)
+result=" ".join(a)  #join 함수를 그간 잘못 이해했다.
+                    #a리스트의 각 단어들을 한 문장으로 조립할 때 단어들 사이 마다 공백을 넣어주는 것" "
+print(result)
